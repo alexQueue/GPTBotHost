680718ad835483e6668fefb59c8e220a76d57690
Alex Quinlan <alexsquinlan@gmail.com>
2022-12-14 17:49:10 +0100

    Remove oldthinking flow

--- app.py
+++ app.py
@@ -17,19 +17,12 @@ async def send_message(message, content, is_private):
 
 # Send messages
 async def send_message(message, user_message, is_private):
-    try:
-        think = False
-        if think:
-            thinking_message = "Thinking. Give me a moment..."
-            reply = await send_message(message, thinking_message, is_private)
+    try:    
+        # await message.channel.trigger_typing()
+        async with message.channel.typing():
             response = davinci_client.handle_response(user_message)
-            await reply.edit(content=response)
-        else:
-            # await message.channel.trigger_typing()
-            async with message.channel.typing():
-                response = davinci_client.handle_response(user_message)
-            
-            await send_message(message, response, is_private)
+        
+        await send_message(message, response, is_private)
 
 
         # streamed
